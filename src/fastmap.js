789633d9d45b04da45e42f467bf5ab3eb47c2fa2
Ryan Morr <rm.morr@gmail.com>
2016-09-08 14:41:16 -0400

    Create implementation

--- src/fastmap.js
+++ src/fastmap.js
@@ -1,10 +1,27 @@
 /**
- * Create an accelerated map
+ * Define a constructor with a null prototype,
+ * instantiating this is much faster than explicitly
+ * calling `Object.create(null)` to get a "bare"
+ * empty object
+ */
+function EmptyObject() {}
+EmptyObject.prototype = Object.create(null);
+
+/**
+ * Create an accelerated hash map
  *
- * @param {...Object} prop
+ * @param {...Object} props (optional)
  * @return {Object}
  * @api public
  */
 export default function fastmap(...props) {
-    
+    const map = new EmptyObject();
+    if (props.length) {
+        props.forEach((object) => {
+            for (const key in object) {
+                map[key] = object[key];
+            }
+        });
+    }
+    return map;
 }
--- test/test-fastmap.js
+++ test/test-fastmap.js
@@ -2,7 +2,55 @@ import { expect } from 'chai';
 import fastmap from '../src/fastmap';
 
 describe('fastmap', () => {
-    it('', () => {
+    it('should be an object', () => {
+        const map = fastmap();
+        expect(map).to.be.an('object');
+    });
 
+    it('should support assigning and reading properties', () => {
+        const map = fastmap();
+        map.foo = 1;
+        map.bar = 2;
+
+        expect(map['foo']).to.equal(1); // eslint-disable-line dot-notation
+        expect(map.bar).to.equal(2);
+    });
+
+    it('should support enumeration', () => {
+        const map = fastmap();
+        map.foo = 1;
+        map.bar = 2;
+
+        let i = 0;
+        const keys = Object.keys(map);
+        for (const prop in map) {
+            expect(prop).to.equal(keys[i]);
+            expect(map[prop]).to.equal(map[keys[i]]);
+            i++;
+        }
+    });
+
+    it('should not have inherited properties/methods', () => {
+        const map = fastmap();
+        let hasProps = false;
+        /* eslint-disable no-unused-vars */
+        for (const key in map) {
+            hasProps = true;
+        }
+        /* eslint-enable no-unused-vars */
+        expect(hasProps).to.equal(false);
+        Object.getOwnPropertyNames(Object.prototype).forEach((prop) => {
+            expect(prop in map).to.equal(false);
+        });
+    });
+
+    it('should allow object literal as an argument to populate the map with its properties', () => {
+        const map = fastmap({foo: 1, bar: 2});
+        expect(map).to.eql({foo: 1, bar: 2});
+    });
+
+    it('should allow multiple object literals as parameters (right overwrites left)', () => {
+        const map = fastmap({foo: 1, bar: 2}, {baz: 3, qux: 4}, {foo: 10});
+        expect(map).to.eql({foo: 10, bar: 2, baz: 3, qux: 4});
     });
 });

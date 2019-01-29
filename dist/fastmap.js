f0f4ee60611e48f38782c5f15536f4e934918686
Ryan Morr <rm.morr@gmail.com>
2019-01-29 13:54:06 -0500

    Build v0.1.1

--- dist/fastmap.js
+++ dist/fastmap.js
@@ -1,11 +1,12 @@
-/*! fastmap v0.1.0 | https://github.com/ryanmorr/fastmap */
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.fastmap = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*! @ryanmorr/fastmap v0.1.1 | https://github.com/ryanmorr/fastmap */
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ryanmorrfastmap = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.default = fastmap;
+
 /**
  * Define a constructor with a null prototype,
  * instantiating this is much faster than explicitly
@@ -13,8 +14,8 @@ exports.default = fastmap;
  * empty object
  */
 function EmptyObject() {}
-EmptyObject.prototype = Object.create(null);
 
+EmptyObject.prototype = Object.create(null);
 /**
  * Create an accelerated hash map
  *
@@ -22,23 +23,22 @@ EmptyObject.prototype = Object.create(null);
  * @return {Object}
  * @api public
  */
+
 function fastmap() {
-    var map = new EmptyObject();
+  var map = new EmptyObject();
 
-    for (var _len = arguments.length, props = Array(_len), _key = 0; _key < _len; _key++) {
-        props[_key] = arguments[_key];
-    }
+  for (var _len = arguments.length, props = new Array(_len), _key = 0; _key < _len; _key++) {
+    props[_key] = arguments[_key];
+  }
 
-    if (props.length) {
-        props.forEach(function (object) {
-            for (var key in object) {
-                map[key] = object[key];
-            }
-        });
-    }
-    return map;
+  if (props.length) {
+    Object.assign.apply(Object, [map].concat(props));
+  }
+
+  return map;
 }
-module.exports = exports["default"];
+
+module.exports = exports.default;
 
 },{}]},{},[1])(1)
 });

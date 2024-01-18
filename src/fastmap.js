77c6eebe5868a5167da196d3884121723ab3977b
Ryan Morr <rm.morr@gmail.com>
2024-01-18 14:26:21 -0500

    Fix dumb typo

--- src/fastmap.js
+++ src/fastmap.js
@@ -3,7 +3,7 @@ EmptyObject.prototype = Object.create(null);
 
 export default function fastmap(props) {
     const map = new EmptyObject();
-    if (props.length) {
+    if (props) {
         Object.assign(map, props);
     }
     return map;
